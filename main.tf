9fd5e562c0767aa6fb81d189280cb91c595d8e32
Anees Shaik <anees.shaik9@gmail.com>
2025-03-24 20:51:52 -0400

    code refactor with vars

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = var.ami_filter.name
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = var.ami_filter.owner
 }
 
 
@@ -19,14 +19,14 @@ module "module_aws_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "blog_vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = "${var.environment.name}"
   }
 }
 
@@ -59,6 +59,8 @@ module "module_aws_alb" {
   vpc_id          = module.module_aws_vpc.vpc_id
   subnets         = module.module_aws_vpc.public_subnets
   security_groups = [module.module_aws_security_group.security_group_id]
+  
+  enable_deletion_protection = true
 
   listeners = {
     hp-http = {
